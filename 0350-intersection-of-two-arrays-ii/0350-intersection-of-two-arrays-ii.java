e9832de83927cb3af0929ceb0b63c454e3c21706
rakeshroshanray <125191960+rakeshroshanray@users.noreply.github.com>
2024-07-02 17:01:34 +0530

    Time: 127 ms (5.42%), Space: 44.2 MB (6.69%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -47,16 +47,4 @@ class Solution {
 
         return intersection;
     }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        int[] nums1 = {1, 2, 2, 1};
-        int[] nums2 = {2, 2};
-        System.out.println(Arrays.toString(solution.intersect(nums1, nums2))); // Output: [2, 2]
-
-        int[] nums1b = {4, 9, 5};
-        int[] nums2b = {9, 4, 9, 8, 4};
-        System.out.println(Arrays.toString(solution.intersect(nums1b, nums2b))); // Output: [4, 9]
-    }
 }
