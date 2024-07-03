f0ed8babea41778d00237d7f616d865f09da4fa6
rakeshroshanray <125191960+rakeshroshanray@users.noreply.github.com>
2024-07-03 23:02:25 +0530

    Time: 5 ms (24%), Space: 44.2 MB (6.73%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -2,11 +2,10 @@ import java.util.*;
 
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        // Create HashMaps to count the elements in both arrays
+       
         Map<Integer, Integer> map1 = new HashMap<>();
         Map<Integer, Integer> map2 = new HashMap<>();
 
-        // Fill the HashMaps with the counts of each element
         for (int num : nums1) {
             map1.put(num, map1.getOrDefault(num, 0) + 1);
         }
@@ -15,7 +14,7 @@ class Solution {
             map2.put(num, map2.getOrDefault(num, 0) + 1);
         }
 
-        // Determine which map is smaller
+        
         if (map1.size() > map2.size()) {
             return intersectHelper(map2, map1);
         } else {
@@ -26,7 +25,7 @@ class Solution {
     private int[] intersectHelper(Map<Integer, Integer> smallerMap, Map<Integer, Integer> largerMap) {
         List<Integer> result = new ArrayList<>();
 
-        // Iterate through the smaller map and find intersections
+
         for (Map.Entry<Integer, Integer> entry : smallerMap.entrySet()) {
             int num = entry.getKey();
             int countInSmaller = entry.getValue();
@@ -39,7 +38,7 @@ class Solution {
             }
         }
 
-        // Convert the result list to an array
+       
         int[] intersection = new int[result.size()];
         for (int i = 0; i < result.size(); i++) {
             intersection[i] = result.get(i);
