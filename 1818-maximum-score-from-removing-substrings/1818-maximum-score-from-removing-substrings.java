20c2a79d8a275b784649b472564c48f6c184b0ec
rakeshroshanray <125191960+rakeshroshanray@users.noreply.github.com>
2024-07-12 22:06:36 +0530

    Time: 2691 ms (5.05%), Space: 47.3 MB (26.26%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -1,10 +1,9 @@
 class Solution {
+
     int value = 0;
-    
     public int maximumGain(String s, int x, int y) {
         
-        
-        if(x >= y){
+        if(x > y){
             String str =  helper(s,"ab",x);
             helper(str,"ba",y);
             
@@ -22,24 +21,19 @@ class Solution {
         while( i < s.length()){
             if (!stack.isEmpty() && stack.peek() == target.charAt(0) && s.charAt(i) == target.charAt(1)) {
 
-                    stack.pop();
-                    value += val;
+                stack.pop();
+                value += val;
             }
             else{
-                    stack.push(s.charAt(i));
+                stack.push(s.charAt(i));
             }
             i++;
-
         }
 
         StringBuilder remaning = new StringBuilder();
-
         while(!stack.isEmpty()){
             remaning.insert(0,stack.pop());
-            // System.out.println(remaning);
         }
-
         return remaning.toString();
-
     }
 }
